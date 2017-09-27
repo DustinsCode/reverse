ebca833bbc3f383359f6c1dd78d00adbf8b321e3
xStaticVoid <dthurston1996@gmail.com>
2017-09-27 14:51:37 -0400

    quicksave

--- reverse.c
+++ reverse.c
@@ -3,5 +3,15 @@
 #include <sys/stat.h>
 
 int main(int argc, char** argv){
-    
+    //argv[1] = file to be read
+    //argv[2] = file to be written
+
+    //Get size of file
+    struct stat st;
+    stat(argv[1], &st);
+    int size = st.st_size;
+
+    char** buffer = malloc(50*sizeof(size));
+    int read = read_file(argv[1], **buffer);
+
 }
