32f4fb23c5eaa9fc2f7b725558e35c7c948f4aaa
Dustin Thurston <dthurston1996@gmail.com>
2017-09-27 15:31:52 -0400

    We have compilation.  Neat.

--- file_utils.c
+++ file_utils.c
@@ -11,17 +11,11 @@ int read_file( char* filename, char **buffer ){
     //This is a file stream and opens it.  "r" = open existing file for reading
     FILE* ogFile = fopen(filename, "r");
 
-    //Get size of string
-    struct stat st;
-    stat(filename, &st);
-    int size = st.st_size;
-
-    //Allocate memory for the buffer
-    *buffer = malloc(size * sizeof(char));
-
+    /**
     for(int i = 0; i < size; i++){
         buffer[i] = ogFile
     }
+    */ 
 
     return 0;
 }
--- reverse.c
+++ reverse.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "file_utils.h"
 #include <sys/stat.h>
 
 int main(int argc, char** argv){
     
+    //Get size of string
+    struct stat st;
+    stat(argv[1], &st);
+    int size = st.st_size;
+
+    char** buffer = malloc(size * sizeof(char));
+
+    int read = read_file(argv[1], buffer);
+    printf("%d",size);
 }
